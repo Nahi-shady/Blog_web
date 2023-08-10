091c1c753223c588df6038419b0b82f005808fa4
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-10 15:48:09 +0800

    Added date-time fields and Meta class  for Post model, also registered blog app in settings.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +7,13 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField(max_length=150)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']),]
 
     def __str__(self):
         return self.title
