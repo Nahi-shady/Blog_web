74f44db879eed19f930bdc0723ac419752b486f8
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-11 01:13:06 +0800

    Created a view for listing all posts and one post in detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except PostDoesNotExist:
+        raise Http404("Post does not exist")
+    
+    return render(request, 'blog/post/detail.html', {'post', post})
