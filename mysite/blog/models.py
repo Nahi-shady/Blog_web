901b188018373d831162630545ef07b1794c7517
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-10 19:28:56 +0800

    Added a many-to-one relationship between Post model and User model, and made the first migration

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
@@ -9,6 +10,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     slug = models.SlugField(max_length=150)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
