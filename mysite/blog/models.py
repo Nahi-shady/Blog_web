f142a1756603946452d69139df0e4b8e47bb0830
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-15 17:25:58 +0800

    Modified the Post URLs to be SEO freindly

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=150)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    slug = models.SlugField(max_length=150)
+    slug = models.SlugField(max_length=150, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -30,7 +30,10 @@ class Post(models.Model):
         indexes = [models.Index(fields=['-publish']),]
     
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                self.publish.month,
+                                                self.publish.day,
+                                                self.slug,])
 
     def __str__(self):
         return self.title
