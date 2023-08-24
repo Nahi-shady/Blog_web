8f9fb798b015ae59b52fb077d2d06b222b0ae9ff
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-25 02:34:16 +0800

    Created post_share function view to validate, send and render EmailPostForm forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Create your views here.
 class PostListView(ListView):
@@ -18,3 +25,28 @@ def post_detail(request, year,month, day, post):
                                                             publish__day=day)
     
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+
+def post_share(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            form = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{form["name"]} recommended you to read {post.title}'
+            message = f'Read {post.title} at {post_url} \n \n'\
+                      f'{form["name"]}\'s comment: {form["comment"]} \n'\
+                        f'sender\'s email: {form["email"]}'
+            send_mail(subject, message, os.getenv('EMAIL_HOST_USER'), [form['to']])
+            sent = True
+    
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                     'form': form,
+                                                    'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,7 +116,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nahiyo86@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('APP_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
