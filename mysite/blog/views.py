400f4a3d401029207ab0c6e905729269923b4088
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-29 18:17:29 +0800

    Created post_comment view to handle post comment requests

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
+
 
 # Create your views here.
 class PostListView(ListView):
@@ -50,3 +51,19 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                      'form': form,
                                                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+    
