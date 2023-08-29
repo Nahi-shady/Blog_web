ccbad30cf8342433bf336a0b32589f5102750c69
Nahom Zewdu <nahiyo86@gmail.com>
2023-08-30 02:39:36 +0800

    Changed PostListView to post_list function view and added functionality to render posts related to a specific tag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,18 +5,39 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-
+from taggit.models import Tag
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
 # Create your views here.
-class PostListView(ListView):
-    queryset = Post.published.all()
-    template_name = 'blog/post/list.html'
-    context_object_name = 'posts'
-    paginate_by = 2
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     template_name = 'blog/post/list.html'
+#     context_object_name = 'posts'
+#     paginate_by = 2
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1) # retrieves the page no of the current page, defaults to 1 if not found.
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html', {'posts': posts, 'tag': tag})
+
 
 def post_detail(request, year,month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
