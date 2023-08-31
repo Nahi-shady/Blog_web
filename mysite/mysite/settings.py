75466c57d34b938fd7a96c8c9e6e911d9d853ca6
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-01 06:55:46 +0800

    Added sitmap and site to installed apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -17,7 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
