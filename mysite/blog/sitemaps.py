b1da9f603b260e542050c4eabf5197b4de725783
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-05 02:55:55 +0800

    Typo fixed

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -3,9 +3,10 @@ from .models import Post
 
 
 class PostSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 0.9
-def items(self):
-     return Post.published.all()
-def lastmod(self, obj):
-     return obj.updated
+     changefreq = 'weekly'
+     priority = 0.9
+     
+     def items(self):
+          return Post.published.all()
+     def lastmod(self, obj):
+          return obj.updated
